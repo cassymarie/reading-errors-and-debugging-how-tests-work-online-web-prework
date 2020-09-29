9d6a226055abf644add11f4dd2131500e1e7c01a
cassandraortiz <cassy.ortiz5@gmail.com>
2020-09-29 12:51:06 -0600

    Learn.co IDE error, could not push to Repository.  Manually done here

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
 # Add your variables here
+# Add your variables here
+first_number = 9
+second_number = 1
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+#Completed in IDE with Learn, Copy/Pasted here (if you are checking MetaData)
